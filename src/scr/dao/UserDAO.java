65f36d94bd975222db19195284bbc0dad6ff0047
KimSeongIl <ksi4687@nate.com>
2015-12-05 22:55:59 +0900

    아이디 중복체크 완료

--- src/scr/dao/UserDAO.java
+++ src/scr/dao/UserDAO.java
@@ -11,55 +11,55 @@ import scr.util.*;
 public class UserDAO {
 
 	private static UserDAO instance=new UserDAO();
-	
-	
+
+
 	private UserDAO(){
-		
+
 	}
-	
-	
+
+
 	public static UserDAO getInstance(){
 		return instance;
 	}
-	
+
 	public void userAdd(UserDTO user) {
-		
+
 		try (
-			Connection conn=Conn.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("insert into user values(?,?,?,?)");){
+				Connection conn=Conn.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("insert into user values(?,?,?,?)");){
 			pstmt.setInt(1, user.getUid());
-		    pstmt.setString(2, Sha256.encrypt(user.getPassword()));
-		    pstmt.setString(3, user.getName());
-		    pstmt.setString(4, user.getAuth());
-		    pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	    
-	}
-	
-	public void userUpdate(UserDTO user) {
-		
-		try (
-			Connection conn=Conn.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("update user set name=?,password=? where user_id=?");){			
-		    pstmt.setString(1,user.getName());
 			pstmt.setString(2, Sha256.encrypt(user.getPassword()));
-		    pstmt.setInt(3, user.getUid());
-	
-		    pstmt.executeUpdate();
+			pstmt.setString(3, user.getName());
+			pstmt.setString(4, user.getAuth());
+			pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	    
+
 	}
-	
-	
+
+	public void userUpdate(UserDTO user) {
+
+		try (
+				Connection conn=Conn.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("update user set name=?,password=? where user_id=?");){			
+			pstmt.setString(1,user.getName());
+			pstmt.setString(2, Sha256.encrypt(user.getPassword()));
+			pstmt.setInt(3, user.getUid());
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
 	public UserDTO login(UserDTO user){
 		UserDTO dto=null;
 		try(
-			Connection conn=Conn.getConnection();
-			PreparedStatement pstmt=conn.prepareStatement("select user_id,name,auth from user where user_id=? and password=?");){
+				Connection conn=Conn.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement("select user_id,name,auth from user where user_id=? and password=?");){
 			pstmt.setInt(1, user.getUid());
 			pstmt.setString(2, Sha256.encrypt(user.getPassword()));
 			try(ResultSet rs=pstmt.executeQuery();){
@@ -68,13 +68,13 @@ public class UserDAO {
 					dto.setUid(rs.getInt("user_id"));
 					dto.setName(rs.getString("name"));
 					dto.setAuth(rs.getString("auth"));
-					
+
 				}
 			}catch(Exception e){
 				e.printStackTrace();
 			}
-			
-			
+
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -85,34 +85,54 @@ public class UserDAO {
 		try(
 				Connection conn=Conn.getConnection();
 				PreparedStatement pstmt=conn.prepareStatement("update user set password=? where user_id=?");){
-			
+
 			pstmt.setString(1, Sha256.encrypt(user.getPassword()));
 			pstmt.setInt(2, user.getUid());
 			pstmt.executeUpdate();
-			
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-	
 
-public boolean passwordCheck(UserDTO user) {
+
+	public boolean passwordCheck(UserDTO user) {
 		boolean check = false;
 		try (
-			Connection conn=Conn.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("select user_id from user where user_id=? and password=? ");){
+				Connection conn=Conn.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("select user_id from user where user_id=? and password=? ");){
 			pstmt.setInt(1, user.getUid());
-		    pstmt.setString(2, Sha256.encrypt(user.getPassword()));
-		    ResultSet rs=pstmt.executeQuery();
-		    if(rs.next()){		    	
-		    	check=true;
-		    }else{
-	    	
-		    	check=false;
-		    }
+			pstmt.setString(2, Sha256.encrypt(user.getPassword()));
+			ResultSet rs=pstmt.executeQuery();
+			if(rs.next()){		    	
+				check=true;
+			}else{
+
+				check=false;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	   
+		return check;
+	}
+	public boolean overlapCheck(UserDTO user){
+		boolean check=false;
+		try(
+				Connection conn=Conn.getConnection();
+				PreparedStatement pstmt=conn.prepareStatement("select user_id from user where user_id=?");){
+			pstmt.setInt(1, user.getUid());
+			try(ResultSet rs=pstmt.executeQuery();){
+				if(rs.next()){
+					check=true;
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+				return check;
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+			return check;
+		}
 		return check;
 	}
 
@@ -120,7 +140,7 @@ public boolean passwordCheck(UserDTO user) {
 		try(
 				Connection conn=Conn.getConnection();
 				PreparedStatement pstmt=conn.prepareStatement("delete from user where user_id=?");){
-			
+
 			pstmt.setInt(1, user.getUid());
 			pstmt.executeUpdate();
 		}catch(Exception e){
@@ -128,6 +148,6 @@ public boolean passwordCheck(UserDTO user) {
 		}
 		return true;
 	}
-	
+
 
 }
